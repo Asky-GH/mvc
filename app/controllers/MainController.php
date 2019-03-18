fe25eb4cb8e55a0ab8d21abb7cdbe848849e5c2a
Askhat Abishev <asky.gm@gmail.com>
2019-03-18 21:48:16 +0600

    add dayside

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -14,9 +14,9 @@ class MainController
         require 'views/about.php';
     }
 
-    public function dayside()
-    {
-        require 'dayside/index.php';
-    }
+    // public function dayside()
+    // {
+    //     require 'dayside/index.php';
+    // }
 }
 
