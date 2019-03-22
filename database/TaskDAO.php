39c0951defad414d18bf4026f26f4e7121a04101
Askhat Abishev <asky.gm@gmail.com>
2019-03-22 13:52:50 +0600

    fix sort by statud_id

--- database/TaskDAO.php
+++ database/TaskDAO.php
@@ -11,7 +11,7 @@ class TaskDAO
     {
         $pdo = Connection::connect();
         
-        $statement = $pdo->prepare("select * from tasks order by {$sortBy} 
+        $statement = $pdo->prepare("select * from tasks order by {$sortBy}, id 
                                 limit :limit offset :offset");
         $statement->bindValue(':offset', $offset, PDO::PARAM_INT);
         $statement->bindValue(':limit', $limit, PDO::PARAM_INT);
